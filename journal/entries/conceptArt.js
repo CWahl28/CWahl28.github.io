2b1be60974944dd9ac09467782521b1bd3fecc33
CWahl28 <Claranwahl@gmail.com>
2025-05-13 08:07:42 -0400

    i forgot to upload

--- journal/entries/conceptArt.js
+++ journal/entries/conceptArt.js
@@ -8,13 +8,12 @@ function drawFace(x,y){
 	ctx.fill();
 }
 function drawRect(x,y) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#785B31";
     ctx.fillRect(x,y,30,35);
     ctx.fill();
 }
-function drawHood(x,y){
-	ctx.fillStyle = "#785B31";
+function drawHair(x,y){
+	ctx.fillStyle = "#b38d56";
         ctx.beginPath();
         ctx.arc(x,y,17,0,2*Math.PI);
         ctx.fill();
@@ -25,21 +24,56 @@ function bodyPart(x,y,z){
         ctx.arc(x,y,z,0,2*Math.PI);
         ctx.fill();
 }
-function addEye(x,y){
+function eyePupil(x,y){
 	ctx.fillStyle= "#4E3410";
 	ctx.beginPath();
-	ctx.arc(x,y,2,0,2*Math.PI);
+	ctx.arc(x,y,4,0,2*Math.PI);
 	ctx.fill();
 }
+function eyeWhite(x,y){
+        ctx.fillStyle= "White";
+        ctx.beginPath();
+        ctx.arc(x,y,8,0,2*Math.PI);
+        ctx.fill();
+}
+
+function eyeball(x,y){
+	eyeWhite(x+3,y-3);
+	eyePupil(x+5,y-1);
+}
+
 function player(x,y){
 	drawRect(x-18,y+10);
-	drawHood(x-5,y);
+	drawHair(x-3,y-3);
 	drawFace(x,y);
 	bodyPart(x-15,y+3,5);
-	addEye(x-3,y);
-	addEye(x+10,y-1);
 }
 
 player(200,200);
+eyeball(200,200);
 
 
+function completeBox(x,y){
+    ctx.fillStyle = "Blue";
+    ctx.fillRect(x,y,50,50);
+    ctx.fill();
+    ctx.strokeRect(x,y,50,50);
+    ctx.lineWidth= 5;
+    ctx.strokeStyle="Black";
+}
+
+function emptySlot(x,y){
+    ctx.fillStyle = "Black";
+    ctx.fillRect(x,y,50,50);
+    ctx.fill();
+}
+
+function looseBox(x,y){
+    ctx.fillStyle = "Red";
+    ctx.fillRect(x,y,50,50);
+    ctx.fill();
+}
+
+completeBox(130,60);
+looseBox(215,200);
+emptySlot(210,300);
